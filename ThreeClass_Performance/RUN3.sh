39ef77a60eb7a8e92466b13ba2e461470bffd1cb
Tizianop6 <tizianop6@gmail.com>
2024-10-22 12:23:26 +0200

    Added the code to evaluate performance of the baseline DNN on signal

---
+++ ThreeClass_Performance/RUN3.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+
+hostname
+
+export TFDIR=$HOME/work/tensorflow
+
+export TFLIB=/gfsvol01/cms/users/dellaric/work/cms/tensorflow
+
+export PYTHONUSERBASE=$TFLIB/local.py3
+export PATH=$TFLIB/local.py3/bin:$PATH
+export LD_LIBRARY_PATH=$TFLIB/local.py3/lib64
+
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+
+date
+
+#cd $TFDIR/py3
+
+export TF_NUM_INTRAOP_THREADS=16
+
+export PYTHONUNBUFFERED=1
+
+python3 steer_inputs_DNN.py
+
+date
+
+exit
---
+++ ThreeClass_Performance/steer_inputs_DNN_mt.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from numpy import inf
+import keras
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.metrics import roc_auc_score, roc_curve, auc
+from sklearn.model_selection import train_test_split
+from IPython.display import FileLink, FileLinks
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.utils import to_categorical, plot_model
+from tensorflow.keras.callbacks import History, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+import math
+import pickle
+from Training import *
+from Plotting import *
+from GetInputs import *
+import multiprocessing 
+from RankNetworks import *
+from PredictExternal import *
+from functions import *
+#from TrainModelOnPredictions import *
+#from TrainSecondNetwork import *
+#from TrainThirdNetwork import *
+from ExportModel import *
+
+# ignore weird Warnings: "These warnings are visible whenever you import scipy
+# (or another package) that was compiled against an older numpy than is installed."
+# https://stackoverflow.com/questions/40845304/runtimewarning-numpy-dtype-size-changed-may-indicate-binary-incompatibility
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+# ignore weird Warnings: "These warnings are visible whenever you import scipy
+# (or another package) that was compiled against an older numpy than is installed."
+# https://stackoverflow.com/questions/40845304/runtimewarning-numpy-dtype-size-changed-may-indicate-binary-incompatibility
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
+
+
+def multiprocessing_func(mass_mt,parameters,tag):
+    #parameters['exclmasseslist']=[mass_mt]
+    PlotROCsMassAlternative(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='', plotfolder='Plots/'+parameters['preprocess']+'/DNN_'+tag+'/mass_'+str(mass_mt), use_best_model=True, usesignals=[2,4],mass=mass_mt)
+    #print('{} squared results in a/an {} number'.format(x, basic_func(y)))
+    
+
+masseslist=[500,1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000]
+
+#variations = ['NOMINAL','JEC_up','JEC_down','JER_up','JER_down']
+variations = ['NOMINAL']
+merged_str = 'Merged'
+parameters = {
+    'layers':[512,512],
+    'batchsize': 32768,
+    'classes':{0: ['RSGluonToTT'], 1: ['ST'], 2:['WJets','DY']},#
+    #'classes':{0: ['ZprimeDMToTTbarResoIncl'], 1: ['ST'], 2:['WJets','DY']},
+    'name': "Kaluza-Klein gluon",
+    #'name':"Z' dark matter",
+    'masseslist':masseslist,
+    'exclmasseslist': [],
+    'regmethod': 'dropout',
+    'regrate':0.50,
+    'batchnorm': False,
+    'epochs':500,
+    'learningrate': 0.00050,
+    'runonfraction': 1.0,
+    'eqweight': False,
+    'preprocess': 'StandardScaler',
+    'sigma': 1.0, #sigma for Gaussian prior (BNN only)
+    #'inputdir': '../Inputs_UL18_muon/',
+    'bkginputdir': '../Inputs_UL17_muon_lite/',
+    'siginputdir': '../Inputs_UL17_muon_signals_DM_clean/',
+    #        'systvar': variations[ivars],
+    'inputsubdir': 'MLInput/', #path to input files: inputdir + systvar + inputsubdir
+    'prepreprocess': 'RAW' #for inputs with systematics don't do preprocessing before merging all inputs on one,     #FixME: add prepreprocessing in case one does not need to merge inputs,
+
+}
+#testmasseslist=['RSGluonToTT_M-6000','RSGluonToTT_M-500','RSGluonToTT_M-3000']
+#testmasseslist=['RSGluonToTT_M-500']#,'RSGluonToTT_M-3000']
+
+
+#parameters['classes']={0: ['TTbar'], 1: ['ST'], 2:['WJets','DY'], 3 : ['RSGluonToTT']}
+tag_trained=dict_to_str(parameters)
+#classtag_orig = get_classes_tag(parameters)
+
+#for masslabel in testmasseslist:
+    #print('lavorando su: ', masslabel)
+    #parameters['classes']={0: ['TTbar'], 1: ['ST'], 2:['WJets','DY'], 3 : [masslabel]}
+tag = dict_to_str(parameters)
+print('tag: ', tag)
+classtag = get_classes_tag(parameters)
+print('classtag: ', classtag)
+
+########## GetInputs ########
+merged_str = 'Merged'
+
+newtrain=True
+
+for ivars in range(len(variations)):
+     merged_str = merged_str+'__'+variations[ivars]
+     parameters['systvar'] = variations[ivars]
+     # # # # # # Get all the inputs
+     # # # # # # # # # ==================
+     inputfolder = 'output/'+parameters['inputsubdir']+parameters['systvar']+'/'+parameters['prepreprocess']+'/'+ classtag
+     if False:
+         GetInputsDifferentMasses(parameters)
+         #PlotInputs(parameters, inputfolder=inputfolder, filepostfix='', plotfolder='Plots/'+parameters['prepreprocess']+'/InputDistributions/'+parameters['systvar']+'/' + classtag)
+  
+parameters['inputdir']='output/'
+#outputfolder_orig_applyprep=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag_orig
+if False:
+    MixInputsDifferentMasses(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, variations=variations, filepostfix='')
+    SplitInputsDifferentMasses(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='')
+    #FitPrepocessingParametrized(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='')
+    ApplyPrepocessingAlreadyTrained(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='',setid='train')
+    ApplyPrepocessingAlreadyTrained(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='',setid='test')
+    ApplyPrepocessingAlreadyTrained(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='',setid='val')
+
+# ApplyPrepocessingTest(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='',setid='train',outputfolder_orig=outputfolder_orig_applyprep)
+# ApplyPrepocessingTest(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='',setid='test',outputfolder_orig=outputfolder_orig_applyprep)
+# ApplyPrepocessingTest(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='',setid='val',outputfolder_orig=outputfolder_orig_applyprep)
+#ApplySignalPrepocessing(parameters, outputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag, filepostfix='')
+
+inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/' + classtag
+outputfolder='output/'+parameters['preprocess']+'/'+merged_str+'/' + classtag+'/DNN_'+tag
+plotfolder = 'Plots/'+parameters['preprocess']
+if newtrain:
+    ##s#PlotInputs(parameters, inputfolder=inputfolder, filepostfix='', plotfolder=plotfolder+'/InputDistributions/'+merged_str+'/' + classtag)
+
+    #TrainNetworkExclMass(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag)
+    PredictExternalBaseline(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='')
+    
+    PredictExternalMassBaseline(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='',masses=masseslist)
+    
+    PlotPerformanceBaseline(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='', plotfolder='Plots/'+parameters['preprocess']+'/DNN_'+tag, use_best_model=True, usesignals=[2,4])
+    #PredictExternalExclMass(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='')
+    #PlotPerformanceExclMass(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='', plotfolder='Plots/'+parameters['preprocess']+'/DNN_'+tag, use_best_model=True, usesignals=[2,4])
+    #PredictExternalMass(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='',masses=masseslist)
+processes=[]
+for i in range(len(masseslist)):
+    p=multiprocessing.Process(target=multiprocessing_func, args=(masseslist[i],parameters,tag))
+    processes.append(p)
+    p.start()
+    if i==5 or i==len(masseslist):
+        for process in processes:
+            process.join()
+        processes=[]
+     
+    #PlotPerformanceMass(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='', plotfolder='Plots/'+parameters['preprocess']+'/DNN_'+tag+'/mass_'+str(masseslist[i]), use_best_model=True, usesignals=[2,4],mass=masseslist[i])
+
+# PredictExternalAlreadyTrained(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='',outputfolder_trained='output/'+parameters['preprocess']+'/DNN_'+tag_trained)
+# PlotPerformanceTest(parameters, inputfolder=parameters['inputdir']+parameters['preprocess']+'/'+merged_str+'/'+classtag, outputfolder='output/'+parameters['preprocess']+'/DNN_'+tag, filepostfix='', plotfolder='Plots/'+parameters['preprocess']+'/DNN_'+tag, use_best_model=True, usesignals=[2,4],outputfolder_orig='output/'+parameters['preprocess']+'/DNN_'+tag_trained)
+# #ExportModel(parameters, inputfolder='input/', outputfolder='output/', use_best_model=True)
+for process in processes:
+    process.join()
+
+PlotMetrics(parameters,tag)
+
+
+
+
